adfdf410816a2716cc28a5844710b0ff7815ae4b
y1204877565 <49982521+y1204877565@users.noreply.github.com>
2019-06-23 23:29:51 +0800

    Add files via upload

---
+++ java/src/Score.java
@@ -0,0 +1,21 @@
+public class Score {
+    String num;
+    String name;
+    double score;
+	
+    public Score(){
+		
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+}
+
